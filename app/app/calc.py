a8e9c9aea9737e2dd0abcded58d99d51d4ba8b49
manish <manishdelhi0085@gmail.com>
2023-07-30 19:37:51 +0530

    configured docker compose and checks for wait for db

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+Calculator function
+"""
+
+def add(x, y):
+    """Add x and y and return results."""
+    return x + y
+
+def substract(x,y):
+    """Subtract x from y and return result"""
+    return y - x
